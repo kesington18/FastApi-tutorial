f26fca50682f042e01d69c7df148208487c6e4e9
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-05-11 05:53:51 +0100

    done with the tutorial, we explored middlewares and cors

--- travFastApi.py
+++ travFastApi.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
+from app.middleware.timer import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
 from sentry_sdk.envelope import Item
 
 app = FastAPI()
@@ -42,5 +44,15 @@ def create_item(item: dict):
         "response": "success",
         'data': items
     }'''
+
+app.middleware("http")(timing_middleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(issues_router)
