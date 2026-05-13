c1cfc6f65ff9d36050d138b95b734150156651c9
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-05-13 11:38:31 +0100

    added versioning, filtering and pagination

--- App.py
+++ App.py
@@ -8,22 +8,44 @@ app = FastAPI()
 class Student(BaseModel):
     id: int
     name: str
+    age: int
     course: str
 
 # Our in-memory list
 students_db = []
 
+
 # GET: See all students
-@app.get("/students", status_code=status.HTTP_200_OK)
-def get_students():
-    return {
-        "success":True,
-        "message": "Success",
-        "data": students_db
-    }
+@app.get("/api/v1/students", status_code=status.HTTP_200_OK)
+def get_students(id: int | None = None,name: str | None = None,age: int | None = None,course: str | None = None,skip: int = 0, limit: int = 10):
+
+    result = students_db
+
+    if id is not None:
+        result = [s for s in students_db if s["id"] == id]
+    if name is not None:
+        result = [s for s in students_db if s["name"] == name]
+    if age is not None:
+        result = [s for s in students_db if s["age"] == age]
+    if course is not None:
+        result = [s for s in students_db if s["course"] == course]
+
+    result = result[skip : limit]
+
+    if result == []:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="no students has been added"
+        )
+    else:
+        return {
+            "success": True,
+            "message": "Success",
+            "data": result
+        }
 
 # POST: Add a student using the Body Model
-@app.post("/students", status_code=status.HTTP_201_CREATED)
+@app.post("/api/v1/students", status_code=status.HTTP_201_CREATED)
 def create_student(student: Student):
     # .dict() or .model_dump() converts the object to a dictionary
     students_db.append(student.model_dump())
@@ -34,7 +56,7 @@ def create_student(student: Student):
     }
 
 # GET: Find one student
-@app.get("/Students/{student_id}", status_code=status.HTTP_200_OK)
+@app.get("/api/v1/students/{student_id}", status_code=status.HTTP_200_OK)
 def get_student(student_id: int):
     for s in students_db:
         if s["id"] == student_id:
@@ -49,7 +71,7 @@ def get_student(student_id: int):
     )
 
 # DELETE: Remove a student
-@app.delete("/students/{student_id}", status_code=status.HTTP_200_OK)
+@app.delete("/api/v1/students/{student_id}", status_code=status.HTTP_200_OK)
 def delete_student(student_id: int):
     for s in students_db:
         if s["id"] == student_id:
